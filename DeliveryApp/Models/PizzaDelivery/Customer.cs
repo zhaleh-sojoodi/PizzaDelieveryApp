401b2729e1124a23526aaef800d3a32d209b77d7
Jaleh Sojoodi <jaleh36@gmail.com>
2022-04-29 14:21:38 -0700

    add models

---
+++ DeliveryApp/Models/PizzaDelivery/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DeliveryApp.Models.PizzaDelivery
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public string CustomerAddress { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ DeliveryApp/Models/PizzaDelivery/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DeliveryApp.Models.PizzaDelivery
+{
+    public partial class Pizza
+    {
+        public Pizza()
+        {
+            PizzaOrders = new HashSet<PizzaOrder>();
+        }
+
+        public int PizzaId { get; set; }
+        public string PizzaName { get; set; }
+        public decimal? PizzaAmount { get; set; }
+
+        public virtual ICollection<PizzaOrder> PizzaOrders { get; set; }
+    }
+}
